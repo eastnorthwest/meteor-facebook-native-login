fb58f7b79c8e8a0444670f55cc5e37840e9d3d85
Steve McDonald <dev@eastnorthwest.com>
2017-08-04 06:53:35 -0700

    Update package.js

--- package.js
+++ package.js
@@ -1,14 +1,16 @@
+
 Package.describe({
-  name: 'eastnorthwest:facebook-native-login',
+  name: 'jsep:facebook-native-login',
   version: '0.1.1',
   // Brief, one-line summary of the package.
   summary: 'Use native api to login with Facebook',
   // URL to the Git repository containing the source code for this package.
-  git: 'https://github.com/eastnorthwest/meteor-facebook-native-login.git',
+  git: 'https://github.com/jsep/meteor-facebook-native-login.git',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
 });
+
 
 
 Npm.depends({
